0402787ac8464f719caf47cfc299f49351dfe77c
shiroshan <siroshang@gmail.com>
2023-05-23 11:06:54 +0530

    docs: Add button

---
+++ src/components/UI/Atoms/Button/Button.type.ts
@@ -0,0 +1,7 @@
+
+import { VariantProps } from "class-variance-authority"
+import { buttonVariants } from "./Button";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
